d2dff76da6f7e965d4c38ea092ccddc9e14d01bf
Pusaku <yusaku@rc5.so-net.ne.jp>
2024-01-22 21:27:22 +0900

    dynamodb session

---
+++ src/routes/console/+page.server.ts
@@ -0,0 +1,12 @@
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+export const load = (async ({ locals }) => {
+    if (locals.session.session_id === undefined) {
+        throw error(400, 'session state is invalid');
+    }
+    return {
+        user_id: locals.session.user_id,
+        email: locals.session.email,
+    }
+}) satisfies PageServerLoad;
