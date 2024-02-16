fb2c1db7972e738a42f28fbe568089f2fcdc5a38
Pusaku <yusaku@rc5.so-net.ne.jp>
2024-02-16 12:45:33 +0900

    envpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
 <p><a href="/login/ms">Login</a></p>
+<p>
+{process.env.OIDC_ROOT_URL}
+</p>
+<p>
+{process.env.CLIENT_ID}
+</p>
+<p>
+{process.env.REDIRECT_URL}
+</p>
